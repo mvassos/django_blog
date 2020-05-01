becf51ccfc533162dd71b2908745a5d558deca3f
mvassos <vassosmanny@gmail.com>
2020-05-01 15:46:42 -0700

    creating env settings

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -21,12 +21,13 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'aaib$60)d$qr#+77lm7*504#ye&gvn!t_1nfd)h0c%6y=dxqh)'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+    # ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
